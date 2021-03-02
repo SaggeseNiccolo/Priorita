b17c4d3b20800a504039267cb73c6fbb62104e7e
SaggeseNiccolo <57776735+SaggeseNiccolo@users.noreply.github.com>
2021-03-02 19:56:42 +0100

    Update Contatori.java

--- Priorita/src/main/java/com/priorita/Contatori.java
+++ Priorita/src/main/java/com/priorita/Contatori.java
@@ -6,30 +6,17 @@ package com.priorita;
  */
 public class Contatori implements Runnable {
 
-    private final int id;
     private int contatore = 0;
-
-    public boolean isRun() {
-        return run;
-    }
-
-    public void setRun(boolean run) {
-        this.run = run;
-    }
-
-    public Contatori(int id) {
-        this.id = id;
-    }
 
     @Override
     public void run() {
-        while (NewMain.run == 0) {
-            
+        while (NewMain.RUN == 0) {
+
         }
-        
-        while (NewMain.run == 1) {
+
+        while (NewMain.RUN == 1) {
             contatore++;
-            System.out.println("Thread" + id + " " + contatore);
+            System.out.println(Thread.currentThread().getName() + " " + contatore);
         }
     }
 
